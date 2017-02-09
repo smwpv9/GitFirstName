373a89ab298722c15b33d988a193a8d83950d565
Shayne <smwpv9@mail.missouri.edu>
2017-02-09 11:33:36 -0600

    Part 2 take 2

---
+++ GitFirstName_2/GitFirstName/1Lab/partA.c
@@ -0,0 +1,29 @@
+/*
+ * partA.c
+ *
+ *  Created on: Jan 26, 2017
+ *      Author: smwpv9
+ * Shayne Wadle
+ */
+
+//preprocessor
+#define pi 3.14
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	int radius = -1;
+	while( radius < 0)
+	{
+		printf("Please enter the radius of a circle: ");
+		scanf("%d", &radius);
+		if(radius < 0)
+			printf("The number cannot be negative.\n");
+	}
+	printf("A circle with radius %d has an area of %.2f\n", radius, pi*pow(radius,2.0));
+	printf("A circle with radius %d has a circumference of %.2f\n", radius, pi*2*(float)radius);
+	printf("WE are done and possibly good coders\n");
+	return 0;
+}
+
